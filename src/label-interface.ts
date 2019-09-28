a990d2c92e69ae12afee98d9bdb010b32ed3b8f2
David L. Goldberg <davidlgoldberg@gmail.com>
2019-09-28 12:06:25 -0400

    Initial commit
    - Includes label-interface.ts.
    - Placeholder for tabs.ts.

---
+++ src/label-interface.ts
@@ -0,0 +1,22 @@
+import * as vscode from 'vscode';
+
+export interface LabelEnvironment {
+    keys: Array<string>;
+    settings: any;
+}
+
+export interface Label {
+    // TODO: can I make this | null instead of undefined?
+    keyLabel: string | undefined;
+    textEditor: vscode.TextEditor | null;
+    element: any; // TODO: add back in HTMLElement
+    settings: any;
+    drawLabel(): Label;
+    animateBeacon(input: any): void;
+    jump(): void;
+    destroy(): void;
+}
+
+export interface Labeler {
+    (environment:LabelEnvironment):Array<Label>;
+}
