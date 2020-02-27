566cb307f153fec5b343672e7b42eab69c476889
David L. Goldberg <davidlgoldberg@gmail.com>
2020-02-27 01:54:03 -0500

    Adding in old label-reducer.ts file

---
+++ src/label-reducer.ts
@@ -0,0 +1,21 @@
+import { Label } from './label-interface';
+
+// (PURE FUNCTION)
+//
+// WHEN GIVEN:
+//
+//      1.  AN ARRAY OF LABELS (* SEE BELOW)
+//      (and)
+//      2. A NEW INPUT KEY
+//
+// RETURNS new collection of labels
+// *without* the labels that do not start with the current key
+
+export default function labelReducer(labels: Array<Label>, currentKey: string) {
+    return labels.filter(function(label: Label) {
+        if (!label.keyLabel) {
+            return false;
+        }
+        return label.keyLabel.startsWith(currentKey);
+    });
+}
