de59ae75dde28839fa616d66e08ee572575e6d17
David L. Goldberg <davidlgoldberg@gmail.com>
2020-02-27 02:22:25 -0500

    Add elm build stuff

---
+++ Makefile
@@ -0,0 +1,31 @@
+default: elm typescript
+
+elm:
+	# add target for `npm install uglify-js` or -g?
+	/usr/local/bin/elm make src/elm/StateMachine.elm --output=out/elm/StateMachine.js --optimize
+	node_modules/uglify-js/bin/uglifyjs out/elm/StateMachine.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | node_modules/uglify-js/bin/uglifyjs --mangle --output=out/elm/StateMachine.js
+	# elm-test
+
+elm-debug:
+	/usr/local/bin/elm make src/elm/StateMachine.elm --output=out/elm/StateMachine.js
+	# add target for `npm install -g elm-test` or not -g?
+	elm-test
+
+typescript:
+	# *** needs work for vs code ***
+	npm install
+	# for now typescript gets built with atom-typescript.
+
+graph:
+	# *** needs work for vs code ***
+	# make graph (svg) of architecture
+	node_modules/madge/bin/cli.js --image graph.svg ./dist
+
+test:
+	# *** needs work for vs code ***
+	elm-test
+	apm test
+
+count:
+	# *** needs work for vs code ***
+	rg --files | grep -v \.js$ | grep -v dist | grep -v \.png$ | grep -v \.gif$ | grep -v package-lock.json | xargs wc -l | sort -n
---
+++ src/elm/StateMachine.elm
@@ -0,0 +1,219 @@
+port module StateMachine exposing (Flags, Labels, Model, Msg(..), activeChanged, addKeyToStatus, clearStatus, exit, getLabels, init, key, labelJumped, main, modelAndJumped, modelAndStatus, onKeyPress, reset, resetKeys, resetStatus, setNoMatchStatus, statusChanged, turnOff, turnOn, update, validKeyEntered)
+
+import Char
+import Html as Html exposing (..)
+import Html.Events as Events exposing (..)
+import Json.Decode as Json
+import List exposing (any)
+import String exposing (..)
+
+
+main : Program Flags Model Msg
+main =
+    Platform.worker
+        { init = init
+        , update = update
+        , subscriptions =
+            \model ->
+                Sub.batch
+                    [ getLabels LoadLabels
+                    , key KeyEntered
+                    , reset (Basics.always Reset)
+                    , exit (Basics.always Exit)
+                    ]
+        }
+
+
+type alias Labels =
+    List String
+
+
+
+-- Outbound
+
+
+port activeChanged : Bool -> Cmd msg
+
+
+port statusChanged : String -> Cmd msg
+
+
+port validKeyEntered : String -> Cmd msg
+
+
+port labelJumped : String -> Cmd msg
+
+
+
+-- Inbound
+
+
+port getLabels : (Labels -> msg) -> Sub msg
+
+
+port key : (Int -> msg) -> Sub msg
+
+
+port reset : (() -> msg) -> Sub msg
+
+
+port exit : (() -> msg) -> Sub msg
+
+
+type Msg
+    = LoadLabels Labels
+    | Reset
+    | KeyEntered Int
+    | Exit
+
+
+type alias Model =
+    { active : Bool
+    , keysEntered : String
+    , lastJumped : String
+    , labels : Labels
+    , status : String
+    }
+
+
+type alias Flags =
+    {}
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( { active = False
+      , keysEntered = ""
+      , lastJumped = ""
+      , labels = []
+      , status = ""
+      }
+    , Cmd.none
+    )
+
+
+onKeyPress : (Int -> msg) -> Attribute msg
+onKeyPress tagger =
+    on "keypress" (Json.map tagger keyCode)
+
+
+clearStatus : Model -> Model
+clearStatus model =
+    { model | status = "" }
+
+
+resetStatus : Model -> Model
+resetStatus model =
+    { model | status = "<div id='status-bar-jumpy'>Jumpy: <span class='status'>Jump Mode!</span></div>" }
+
+
+resetKeys : Model -> Model
+resetKeys model =
+    { model | keysEntered = "" }
+
+
+turnOff : Model -> Model
+turnOff model =
+    { model | active = False }
+        |> resetKeys
+        |> clearStatus
+
+
+setNoMatchStatus : Model -> Model
+setNoMatchStatus model =
+    { model | status = "<div id='status-bar-jumpy' class='no-match'>Jumpy: <span>No Match! 😞</span></div>" }
+
+
+addKeyToStatus : String -> Model -> Model
+addKeyToStatus keyEntered model =
+    { model | status = "<div id='status-bar-jumpy'>Jumpy: <span class='status'>" ++ keyEntered ++ "</span></div>" }
+
+
+modelAndStatus : Model -> ( Model, Cmd Msg )
+modelAndStatus model =
+    ( model
+    , Cmd.batch
+        [ activeChanged model.active
+        , statusChanged model.status
+        , validKeyEntered model.keysEntered
+        ]
+    )
+
+
+modelAndJumped : Model -> ( Model, Cmd Msg )
+modelAndJumped model =
+    ( model
+    , Cmd.batch
+        [ activeChanged model.active
+        , statusChanged model.status
+        , labelJumped model.lastJumped
+        ]
+    )
+
+
+turnOn : Model -> Model
+turnOn model =
+    { model | active = True }
+        |> resetStatus
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        KeyEntered keyCode ->
+            let
+                keyEntered =
+                    keyCode |> Char.fromCode |> String.fromChar
+
+                newKeysEntered =
+                    model.keysEntered ++ keyEntered
+
+                keysEnteredMatch =
+                    model.labels
+                        |> List.any (\label -> startsWith newKeysEntered label)
+            in
+            if model.active then
+                if not keysEnteredMatch then
+                    model
+                        |> setNoMatchStatus
+                        |> modelAndStatus
+
+                else
+                    case length model.keysEntered of
+                        0 ->
+                            -- FIRST LETTER ----------
+                            { model | keysEntered = newKeysEntered }
+                                |> addKeyToStatus keyEntered
+                                |> modelAndStatus
+
+                        1 ->
+                            -- SECOND LETTER ----------
+                            { model | lastJumped = newKeysEntered }
+                                |> turnOff
+                                |> modelAndJumped
+
+                        _ ->
+                            ( model, Cmd.none )
+
+            else
+                ( model, Cmd.none )
+
+        Reset ->
+            if model.active then
+                model
+                    |> resetKeys
+                    |> resetStatus
+                    |> modelAndStatus
+
+            else
+                ( model, Cmd.none )
+
+        LoadLabels labels ->
+            { model | labels = labels }
+                |> turnOn
+                |> modelAndStatus
+
+        Exit ->
+            model
+                |> turnOff
+                |> modelAndStatus
