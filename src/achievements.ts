034828509add3902d4730f3fa95602e6782cfcb6
David L. Goldberg <DavidLGoldberg@gmail.com>
2023-10-30 15:24:15 -0400

    :recycle: Pull out sponsor block

--- src/achievements.ts
+++ src/achievements.ts
@@ -1,4 +1,6 @@
 // achievements.ts
+import { sponsoredBlock } from './sponsored';
+
 interface Achievement {
     level: string;
     emoji: string;
@@ -58,20 +60,7 @@ export const achievementsWebview = (careerJumpsMade: number) => {
                     </h2>
                 </div>
                 <hr />
-                <div id="sponsor">
-                    <h2>Maybe it's time to Support Jumpy2?</h2>
-                    <p>
-                    <strong>Buy <a href="https://www.bonfire.com/store/jumpy">Jumpy swag</a>!</strong>
-                    </p>
-                    <p>
-                    <ul style="padding:0;margin:0;list-style:none;">
-                        <li>Subscribe to my youtube channel: <a href="https://www.youtube.com/channel/UCi6p1uTlAozufNiQgpgpW-Q">https://www.youtube.com/channel/UCi6p1uTlAozufNiQgpgpW-Q</a></li>
-                        <li>Sponsor me on Github <a href="https://github.com/sponsors/DavidLGoldberg">David L Goldberg</a></li>
-                        <li>Support me on <a href="https://www.patreon.com/davidlgoldberg">Patreon</a></li>
-                        <li>Support me via <a href="https://github.com/DavidLGoldberg/jumpy2/blob/main/crypto-donations.md">crypto</a></li>
-                    </ul>
-                    </p>
-                </div>
+                ${sponsoredBlock}
                 <hr />
                 <div id="achievements-section">
                     <h2>Achievements</h2>
---
+++ src/sponsored.ts
@@ -0,0 +1,17 @@
+// sponsored.ts
+export const sponsoredBlock = `
+<div id="sponsor">
+    <h2>Maybe it's time to Support Jumpy2?</h2>
+    <p>
+        <strong>Buy <a href="https://www.bonfire.com/store/jumpy">Jumpy swag</a>!</strong>
+    </p>
+    <p>
+        <ul style="padding:0;margin:0;list-style:none;">
+            <li>Subscribe to my youtube channel: <a href="https://www.youtube.com/channel/UCi6p1uTlAozufNiQgpgpW-Q">https://www.youtube.com/channel/UCi6p1uTlAozufNiQgpgpW-Q</a></li>
+            <li>Sponsor me on Github <a href="https://github.com/sponsors/DavidLGoldberg">David L Goldberg</a></li>
+            <li>Support me on <a href="https://www.patreon.com/davidlgoldberg">Patreon</a></li>
+            <li>Support me via <a href="https://github.com/DavidLGoldberg/jumpy2/blob/main/crypto-donations.md">crypto</a></li>
+        </ul>
+    </p>
+</div>
+`;
